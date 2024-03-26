6947143a9d69ee3d96b9f86faaffa0b8c5029075
Hojun Im <ghwnsdla8094@gmail.com>
2024-03-26 22:10:47 +0900

    로그인, 회원가입 담당 컨트롤러 추가

--- src/main/java/com/gachon/learningmate/controller/AuthController.java
+++ src/main/java/com/gachon/learningmate/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.gachon.learningmate.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,4 +12,10 @@ public class AuthController {
     public String showLoginForm() {
         return "login";
     }
+
+    @GetMapping("/register")
+    public String showRegisterFrom() {
+        return "register";
+    }
+
 }
