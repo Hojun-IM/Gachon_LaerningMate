7bbcaa6bd6ecb195ce131cbbc67de2d037af314c
Hojun Im <ghwnsdla8094@gmail.com>
2024-03-26 23:28:16 +0900

    회원가입 post 추가

--- src/main/java/com/gachon/learningmate/controller/AuthController.java
+++ src/main/java/com/gachon/learningmate/controller/AuthController.java
@@ -1,12 +1,24 @@
 package com.gachon.learningmate.controller;
 
+import com.gachon.learningmate.data.dto.RegisterDto;
+import com.gachon.learningmate.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class AuthController {
+
+    private final AuthService authService;
+
+    @Autowired
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
 
     @GetMapping("/login")
     public String showLoginForm() {
@@ -18,4 +30,20 @@ public class AuthController {
         return "register";
     }
 
+    @PostMapping("/register")
+    public String doRegister(@ModelAttribute RegisterDto registerDto, BindingResult result, RedirectAttributes redirectAttributes) {
+        // 유효성 검사 실패 시, 회원가입 화면으로 redirect
+        if (result.hasErrors()) {
+            return "/register";
+        }
+
+        // 회원가입 로직
+        authService.register(registerDto);
+
+        // 성공 메세지를 redirect
+        redirectAttributes.addFlashAttribute("successMessage", "회원가입이 성공적으로 완료되었습니다");
+
+        // 회원가입 성공 후, 로그인 화면으로 redirect
+        return "redirect:/login";
+    }
 }
