bf2ea2703b3933d47917552f2df7ab99a1b46faa
Hojun Im <ghwnsdla8094@gmail.com>
2024-03-31 15:01:22 +0900

    [feat] 문자열로 입력받은 생년월일 Date로 변경로직 추가

--- src/main/java/com/gachon/learningmate/service/AuthService.java
+++ src/main/java/com/gachon/learningmate/service/AuthService.java
@@ -6,6 +6,10 @@ import com.gachon.learningmate.data.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Service
 public class AuthService {
 
@@ -20,12 +24,23 @@ public class AuthService {
     public void register(RegisterDto registerDto) {
         //비밀번호 암호화
 
+        // Birth 문자열을 Date로 변환
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        Date birthDate = null;
+        try {
+            birthDate = dateFormat.parse(registerDto.getBirth());
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
         // registerDto의 사용자 정보를 엔티티 객체로 변환
         User user = User.builder()
                 .userId(registerDto.getUserId())
                 .password(registerDto.getPassword())
-                .username(registerDto.getEmail())
+                .username(registerDto.getUsername())
                 .email(registerDto.getEmail())
+                .birth(birthDate)
+                .type(registerDto.getType())
                 .build();
         
         // 사용자 정보 저장
