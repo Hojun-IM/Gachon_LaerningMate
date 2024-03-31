859eacbdfa2b89338e36931912951900cc188c5f
Hojun Im <ghwnsdla8094@gmail.com>
2024-03-31 22:49:23 +0900

    [fix] birth를 string 타입으로 변경 및 toString 생성

--- src/main/java/com/gachon/learningmate/data/dto/RegisterDto.java
+++ src/main/java/com/gachon/learningmate/data/dto/RegisterDto.java
@@ -2,6 +2,7 @@ package com.gachon.learningmate.data.dto;
 
 import com.gachon.learningmate.data.entity.UserType;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -9,31 +10,44 @@ import org.springframework.format.annotation.DateTimeFormat;
 import java.util.Date;
 
 @Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegisterDto {
 
-    @NotBlank(message = "ID는 필수 항목입니다.")
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,20}$", message = "ID는 6~20자의 영문자와 숫자의 조합이어야 합니다.")
+    @NotBlank(message = "ID는 필수 항목입니다.")
     private String userId;
 
-    @NotBlank(message = "비밀번호는 필수 항목입니다.")
     @Pattern(regexp="(?=.*[0-9])(?=.*[a-z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자의 영문자, 숫자, 특수 문자의 조합이어야 합니다.")
+    @NotBlank(message = "비밀번호는 필수 항목입니다.")
     private String password;
 
-    @NotBlank(message = "이름은 필수 항목입니다.")
     @Pattern(regexp = "^[가-힣a-zA-Z]{3,30}$", message = "이름은 한글 또는 영문자만 가능합니다.")
+    @NotBlank(message = "이름은 필수 항목입니다.")
     private String username;
 
-    @NotBlank(message = "이메일은 필수 항목입니다.")
     @Pattern(regexp = "^[\\w.-]+@gachon\\.ac\\.kr$", message = "가천대학교 이메일을 사용해주세요.")
+    @NotBlank(message = "이메일은 필수 항목입니다.")
     private String email;
 
     @NotBlank(message = "생년월일은 필수 항목입니다.")
-    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    @Pattern(regexp = "^\\d{4}/\\d{2}/\\d{2}$", message = "생일은 yyyy/MM/dd 형식으로 입력해야 합니다.")
     private String birth;
 
     @Builder.Default
     private UserType type = UserType.Member;
+
+    @Override
+    public String toString() {
+        return "RegisterDto{" +
+                "userId='" + userId + '\'' +
+                ", password='" + password + '\'' +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", birth='" + birth + '\'' +
+                ", type=" + type +
+                '}';
+    }
 }
