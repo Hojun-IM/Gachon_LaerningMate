46f8a64c1a25d136cb27ffc1714e937a61c12633
Hojun Im <ghwnsdla8094@gmail.com>
2024-03-31 22:48:19 +0900

    [feat] 회원가입 유효성 검사 추가

--- src/main/java/com/gachon/learningmate/controller/AuthController.java
+++ src/main/java/com/gachon/learningmate/controller/AuthController.java
@@ -6,11 +6,12 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Map;
 
 @Controller
 public class AuthController {
@@ -28,24 +29,23 @@ public class AuthController {
     }
 
     @GetMapping("/register")
-    public String showRegisterFrom() {
-        return "register";
-    }
+    public String showRegisterFrom() { return "register"; }
 
     @PostMapping("/register")
-    public String doRegister(@ModelAttribute @Valid RegisterDto registerDto, BindingResult result, RedirectAttributes redirectAttributes, Model model) {
-        // 유효성 검사 실패 시, 회원가입 화면으로 redirect
-        if (result.hasErrors()) {
-            model.addAttribute("errors", result.getAllErrors());
+    public String doRegister(@Valid RegisterDto registerDto, Errors errors, Model model) {
+
+        if (errors.hasErrors()) {
             model.addAttribute("registerDto", registerDto);
-            return "redirect:/register";
+            // 유효성 검사를 통과 못한 필드와 메시지 핸들링
+            Map<String, String> validatorResult = authService.validateHandling(errors);
+            for (String key : validatorResult.keySet()) {
+                model.addAttribute(key, validatorResult.get(key));
+            }
+            return "register";
         }
 
         // 회원가입 로직
         authService.register(registerDto);
-
-        // 성공 메세지를 redirect
-        redirectAttributes.addFlashAttribute("successMessage", "회원가입이 성공적으로 완료되었습니다");
 
         // 회원가입 성공 후, 로그인 화면으로 redirect
         return "redirect:/login";
