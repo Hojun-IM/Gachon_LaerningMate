73c7fe456e47dfb0363bb458d1e751e63a6df128
Hojun Im <ghwnsdla8094@gmail.com>
2024-04-08 00:08:10 +0900

    [feat] RedisConfig 추가

---
+++ src/main/java/com/gachon/learningmate/Config/RedisConfig.java
@@ -0,0 +1,37 @@
+package com.gachon.learningmate.Config;
+
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+
+    private final RedisProperties properties;
+
+    public RedisConfig(RedisProperties properties) {
+        this.properties = properties;
+    }
+
+    // yml 설정 파일에 저장한 정보와 연결
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(properties.getHost(), properties.getPort());
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+
+        return template;
+    }
+}
