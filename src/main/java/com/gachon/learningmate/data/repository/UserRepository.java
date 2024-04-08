d6926e9ccd6a599ea79675538364780413677ace
Hojun Im <ghwnsdla8094@gmail.com>
2024-04-09 00:05:23 +0900

    [feat] email 검색 추가

--- src/main/java/com/gachon/learningmate/data/repository/UserRepository.java
+++ src/main/java/com/gachon/learningmate/data/repository/UserRepository.java
@@ -8,4 +8,7 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, String> {
     // userId를 통해 User엔티티 조회
     User findByUserId(String userId);
+
+    // email 인증을 위해 이메일 존재 여부 검색
+    boolean existsByEmail(String email);
 }
