bf86d31ab5271eab126a96082a20939e7e9cce31
Hojun Im <ghwnsdla8094@gmail.com>
2024-04-28 19:06:17 +0900

    [feat] 인증코드 생성, 이메일 전송, 인증 코드 확인 기능 추가

--- src/main/java/com/gachon/learningmate/service/AuthService.java
+++ src/main/java/com/gachon/learningmate/service/AuthService.java
@@ -1,5 +1,6 @@
 package com.gachon.learningmate.service;
 
+import com.gachon.learningmate.data.dto.LoginDto;
 import com.gachon.learningmate.data.dto.RegisterDto;
 import com.gachon.learningmate.data.entity.User;
 import com.gachon.learningmate.data.repository.UserRepository;
@@ -9,20 +10,84 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.Errors;
 import org.springframework.validation.FieldError;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.time.Duration;
+import java.util.*;
 
 @Service
 public class AuthService {
 
     private final UserRepository userRepository;
+    private final MailService mailService;
+    private final RedisService redisService;
 
     @Autowired
-    public AuthService(UserRepository userRepository) {
+    public AuthService(UserRepository userRepository, MailService mailService, RedisService redisService) {
         this.userRepository = userRepository;
+        this.mailService = mailService;
+        this.redisService = redisService;
+    }
+
+    // 이메일 유효성 확인
+    public void checkEmail(String email) {
+        // 이메일 양식 확인
+        if (!email.matches("[\\w.-]+@gachon\\.ac\\.kr")){
+            throw new RuntimeException("가천대학교 이메일을 사용해주세요.");
+        }
+
+        // 중복 확인
+        if (userRepository.existsByEmail(email)) {
+            throw new RuntimeException("중복된 이메일 주소입니다.");
+        }
+    }
+
+    // 이메일 인증 코드 생성
+    public String createVerificationCode() {
+        int length = 6;
+        try {
+            Random random = SecureRandom.getInstanceStrong();
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < length; i++) {
+                builder.append(random.nextInt(10));
+            }
+            return builder.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("인증코드 생성에 실패했습니다.");
+        }
+    }
+
+    // 인증 코드 전송
+    @Transactional
+    public void sendVerificationCode(String email) {
+        // 전송 전 이메일 중복 확인
+        try {
+            this.checkEmail(email);
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+        
+        // 인증 코드 생성
+        String verificationCode = createVerificationCode();
+
+        // Redis에 인증번호 저장
+        Duration duration = Duration.ofMinutes(5);
+        redisService.setValues(email, verificationCode, duration);
+        
+        // 메일 전송
+        mailService.sendMail(email, "Gachon LearningMate 이메일 인증 코드", "인증 코드: " + verificationCode);
+    }
+
+    // 인증 코드 확인
+    @Transactional
+    public void verifyCode(String email, String authCode){
+        String savedCode = redisService.getValues(email);
+        if(!authCode.equals(savedCode)){
+            throw new RuntimeException("인증 코드가 올바르지 않습니다.");
+        }
+        redisService.deleteValues(email);
     }
 
     // 회원가입 유효성 검사
@@ -38,10 +103,8 @@ public class AuthService {
         return validatorResult;
     }
 
-    // 회원가입
-    public void register(RegisterDto registerDto) {
-        //비밀번호 암호화
-
+    // 날짜 변환
+    private Date parseDate(RegisterDto registerDto) {
         // birth 문자열을 Date로 변환
         Date birthDate = null;
         if (registerDto.getBirth() != null && !registerDto.getBirth().isEmpty()) {
@@ -49,14 +112,16 @@ public class AuthService {
             try {
                 birthDate = dateFormat.parse(registerDto.getBirth());
             } catch (ParseException e) {
-                // 여기서는 예외를 적절히 처리하거나, 변환 실패 시 사용자에게 알릴 수 있는 메커니즘을 구현해야 합니다.
-                // 예외 처리 또는 사용자에게 전달할 메시지 처리
                 throw new RuntimeException("생년월일 형식이 올바르지 않습니다.");
             }
         }
+        return birthDate;
+    }
 
-        // registerDto의 사용자 정보를 엔티티 객체로 변환
-        User user = User.builder()
+    // 회원가입을 위한 User 객체 생성
+    private User createUserFromDto(RegisterDto registerDto) {
+        Date birthDate = parseDate(registerDto);
+        return User.builder()
                 .userId(registerDto.getUserId())
                 .password(registerDto.getPassword())
                 .username(registerDto.getUsername())
@@ -64,10 +129,18 @@ public class AuthService {
                 .birth(birthDate)
                 .type(registerDto.getType())
                 .build();
+    }
 
+    // 회원가입
+    public void register(RegisterDto registerDto) {
+        //비밀번호 암호화
+
+        // registerDto의 사용자 정보를 엔티티 객체로 변환
+        User user = createUserFromDto(registerDto);
         // 사용자 정보 저장
         userRepository.save(user);
     }
 
     // 로그인 로직
+
 }
