577b1e03dd972491f4c6d82cdcc08fefe96e9e3d
Hojun Im <ghwnsdla8094@gmail.com>
2024-04-28 15:37:09 +0900

    [feat] 아이디와 비밀번호 필드 추가

--- src/main/java/com/gachon/learningmate/data/dto/LoginDto.java
+++ src/main/java/com/gachon/learningmate/data/dto/LoginDto.java
@@ -1,5 +1,6 @@
 package com.gachon.learningmate.data.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Getter
@@ -8,4 +9,10 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginDto {
+
+    @NotBlank(message = "ID를 입력해주세요.")
+    private String userId;
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    private String password;
 }
