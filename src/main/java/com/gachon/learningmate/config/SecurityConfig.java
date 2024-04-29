8cfae9ec5bd0dca7b34956d54af5174f719eb870
Hojun Im <ghwnsdla8094@gmail.com>
2024-04-29 21:14:38 +0900

    [feat] 정적 파일 접근 허용

--- src/main/java/com/gachon/learningmate/config/SecurityConfig.java
+++ src/main/java/com/gachon/learningmate/config/SecurityConfig.java
@@ -18,18 +18,19 @@ public class SecurityConfig {
                 .authorizeHttpRequests((authrizeRequest) ->
                         authrizeRequest
                                 .requestMatchers("/", "/home", "/login", "/registerFirst", "/register/send-verification", "/register/verify-code").permitAll()  // 로그인 없이 접근 허용
-                                .requestMatchers("/registerSecond").authenticated()  // 인증된 사용자만 접근 허용
-                                .anyRequest().authenticated()  // 그 외 모든 요청은 인증 필요
+                                .requestMatchers("/registerSecond").authenticated()              // 인증된 사용자만 접근 허용
+                                .requestMatchers("/css/**", "/js/**", "/img/**").permitAll() // 정적 파일 접근 허용
+                                .anyRequest().authenticated()                                      // 그 외 모든 요청은 인증 필요
                 )
                 .formLogin((formLogin) ->
                         formLogin
                             .loginPage("/login")  // 사용자 정의 로그인 페이지
-                            .permitAll()  // 로그인 페이지 접근 허용
+                            .permitAll()          // 로그인 페이지 접근 허용
                 )
                 .logout((logout) ->
                         logout
                             .logoutSuccessUrl("/home")  // 로그아웃 성공 시 리다이렉트할 페이지
-                            .permitAll() // 로그인 페이지 접근 허용
+                            .permitAll()                // 로그인 페이지 접근 허용
                 )
                 .csrf((csrfConfig) ->
                         csrfConfig.disable()// CSRF 보호 기능 비활성화
