c856e55a0c05c67503178ef3df559d9487681009
Hojun Im <ghwnsdla8094@gmail.com>
2024-04-29 17:39:39 +0900

    [fix] 이메일 인증번호 전송 권한 추가

--- src/main/java/com/gachon/learningmate/config/SecurityConfig.java
+++ src/main/java/com/gachon/learningmate/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,7 +17,7 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests((authrizeRequest) ->
                         authrizeRequest
-                                .requestMatchers("/", "/home", "/login", "/registerFirst").permitAll()  // 로그인 없이 접근 허용
+                                .requestMatchers("/", "/home", "/login", "/registerFirst", "/register/send-verification").permitAll()  // 로그인 없이 접근 허용
                                 .requestMatchers("/registerSecond").authenticated()  // 인증된 사용자만 접근 허용
                                 .anyRequest().authenticated()  // 그 외 모든 요청은 인증 필요
                 )
@@ -34,4 +36,5 @@ public class SecurityConfig {
                 );
         return http.build();
     }
+
 }
