56cda72300f461c4a0b1c9cee0bf451369c7053c
Hojun Im <ghwnsdla8094@gmail.com>
2024-05-07 00:06:20 +0900

    [Add] CustomAuthenticationFailureHandler 추가

---
+++ src/main/java/com/gachon/learningmate/handler/CustomAuthenticationFailureHandler.java
@@ -0,0 +1,25 @@
+package com.gachon.learningmate.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        // 오류 메시지 인코딩
+        String errorMessage = exception.getMessage();
+        String encodedMessage = URLEncoder.encode(errorMessage, StandardCharsets.UTF_8);
+
+        // 예외 메시지를 쿼리 매개변수로 추가
+        response.sendRedirect("/login?error=true&message=" + encodedMessage);
+    }
+
+}
