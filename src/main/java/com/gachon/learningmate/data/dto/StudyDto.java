70d506fb54b0335197db26a4c05c31b853852d37
Hojun Im <ghwnsdla8094@gmail.com>
2024-05-07 22:23:11 +0900

    [feat] StudyDto 추가

---
+++ src/main/java/com/gachon/learningmate/data/dto/StudyDto.java
@@ -0,0 +1,50 @@
+package com.gachon.learningmate.data.dto;
+
+import com.gachon.learningmate.data.entity.StudyStatus;
+import com.gachon.learningmate.data.entity.User;
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudyDto {
+
+    @NotBlank(message = "생성자 ID는 필수 항목입니다.")
+    private User creatorId;
+
+    @NotBlank(message = "스터디명은 필수 항목입니다.")
+    @Size(min = 2, max = 15, message = "스터디명은 2글자에서 15글자 사이여야 합니다.")
+    private String studyName;
+
+    @NotBlank(message = "스터디 설명은 필수 항목입니다.")
+    @Size(min = 10, message = "스터디 설명은 최소 10글자 이상이어야 합니다.")
+    private String description;
+
+    @NotNull(message = "스터디 모집 상태는 필수 항목입니다.")
+    private StudyStatus status;
+
+    @NotBlank(message = "스터디 카테고리는 필수 항목입니다.")
+    private String category;
+
+    @NotBlank(message = "활동 장소는 필수 항목입니다.")
+    @Size(min = 2, message = "활동 장소는 최소 2글자 이상이어야 합니다.")
+    private String location;
+
+    @NotNull(message = "최대 활동 멤버는 필수 항목입니다.")
+    @Min(value = 1, message = "최소 멤버 수는 1명입니다.")
+    @Max(value = 50, message = "최대 멤버 수는 50명입니다.")
+    private int maxMember;
+
+    @Min(value = 1, message = "현재 멤버 수는 최소 1명입니다.")
+    private int currentMember;
+
+    // 현재 멤버 수가 최대 멤버 수 초과 불가
+    @AssertTrue(message = "현재 멤버 수는 최대 멤버 수를 초과할 수 없습니다.")
+    public boolean isCurrentMemberValid() {
+        return currentMember <= maxMember;
+    }
+
+}
