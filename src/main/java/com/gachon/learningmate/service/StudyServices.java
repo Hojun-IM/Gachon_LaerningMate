1bd6b7c845956651e68b307f573419b4db775244
Hojun Im <ghwnsdla8094@gmail.com>
2024-05-07 22:34:43 +0900

    [feat] StudyServices 추가

---
+++ src/main/java/com/gachon/learningmate/service/StudyServices.java
@@ -0,0 +1,38 @@
+package com.gachon.learningmate.service;
+
+import com.gachon.learningmate.data.dto.StudyDto;
+import com.gachon.learningmate.data.entity.Study;
+import com.gachon.learningmate.data.repository.StudyRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudyServices {
+
+    private final StudyRepository studyRepository;
+
+    @Autowired
+    public StudyServices(StudyRepository studyRepository) {
+        this.studyRepository = studyRepository;
+    }
+
+    // 스터디 생성
+    @Transactional
+    public void createStudy(StudyDto studyDto) {
+
+        Study study = Study.builder()
+                .creatorId(studyDto.getCreatorId())
+                .studyName(studyDto.getStudyName())
+                .description(studyDto.getDescription())
+                .status(studyDto.getStatus())
+                .category(studyDto.getCategory())
+                .location(studyDto.getLocation())
+                .maxMember(studyDto.getMaxMember())
+                .currentMember(studyDto.getCurrentMember())
+                .build();
+
+        studyRepository.save(study);
+    }
+
+}
