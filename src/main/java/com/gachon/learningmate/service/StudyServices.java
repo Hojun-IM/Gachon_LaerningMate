17a0171544e2b24f19c9d8cfcd3d01715d1c7bb8
Hojun Im <ghwnsdla8094@gmail.com>
2024-05-08 11:03:54 +0900

    [feat] 스터디 업데이트

--- src/main/java/com/gachon/learningmate/service/StudyServices.java
+++ src/main/java/com/gachon/learningmate/service/StudyServices.java
@@ -2,6 +2,7 @@ package com.gachon.learningmate.service;
 
 import com.gachon.learningmate.data.dto.StudyDto;
 import com.gachon.learningmate.data.entity.Study;
+import com.gachon.learningmate.data.entity.User;
 import com.gachon.learningmate.data.repository.StudyRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,5 +42,42 @@ public class StudyServices {
 
         studyRepository.save(study);
     }
+
+    // 스터디 업데이트
+    @Transactional
+    public Study updateStudy(int studyId, StudyDto studyDto, User currentUser) {
+        Study study = studyRepository.findById(studyId);
+
+        // 스터디 존재 여부 확인
+        if (study == null) {
+            throw new IllegalArgumentException("해당 스터디를 찾을 수 없습니다.");
+        }
+
+        // 사용자 존재 여부 확인
+        if (currentUser == null) {
+            throw new IllegalArgumentException("회원을 찾을 수 없습니다.");
+        }
+
+        // 변경하고자 하는 스터디 생성자인지 확인
+        if (!studyDto.getCreatorId().equals(study.getCreatorId())) {
+            throw new IllegalStateException("스터디를 업데이트할 권한이 없습니다.");
+        }
+
+        // 변경 정보 업데이트
+        study = Study.builder()
+                .creatorId(studyDto.getCreatorId())
+                .studyName(studyDto.getStudyName())
+                .description(studyDto.getDescription())
+                .status(studyDto.getStatus())
+                .category(studyDto.getCategory())
+                .location(studyDto.getLocation())
+                .maxMember(studyDto.getMaxMember())
+                .currentMember(studyDto.getCurrentMember())
+                .build();
+
+        return studyRepository.save(study);
+    }
+
+    // 스터디 삭제
 
 }
