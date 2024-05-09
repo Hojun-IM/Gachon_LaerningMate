487f6874ee8723e590e5a31f3a26bbacb5311f64
Hojun Im <ghwnsdla8094@gmail.com>
2024-05-09 23:23:21 +0900

    [feat] StudyController

---
+++ src/main/java/com/gachon/learningmate/controller/StudyController.java
@@ -0,0 +1,22 @@
+package com.gachon.learningmate.controller;
+
+import com.gachon.learningmate.data.repository.StudyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StudyController {
+
+    private final StudyRepository studyRepository;
+
+    @Autowired
+    public StudyController(StudyRepository studyRepository) {
+        this.studyRepository = studyRepository;
+    }
+
+    @GetMapping("/study")
+    public String showStudyList() {
+        return "study";
+    }
+}
