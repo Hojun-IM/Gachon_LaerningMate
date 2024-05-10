f4f59863722d5c7de3999bfd1adf30692c455040
Hojun Im <ghwnsdla8094@gmail.com>
2024-05-11 00:54:51 +0900

    [feat] 스터딧 생성 뷰에 사용자명, 이메일 자동 등록

--- src/main/java/com/gachon/learningmate/controller/StudyController.java
+++ src/main/java/com/gachon/learningmate/controller/StudyController.java
@@ -1,9 +1,16 @@
 package com.gachon.learningmate.controller;
 
+import com.gachon.learningmate.data.dto.UserPrincipalDetails;
+import com.gachon.learningmate.data.entity.Study;
 import com.gachon.learningmate.data.repository.StudyRepository;
+import org.springframework.security.core.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudyController {
@@ -20,8 +27,19 @@ public class StudyController {
         return "study";
     }
 
-    @GetMapping("/register-study")
-    public String showRegisterStudy() {
-        return "registerStudy";
+    @GetMapping("/study-create")
+    public String showCreateStudy(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserPrincipalDetails userPrincipalDetails = (UserPrincipalDetails) authentication.getPrincipal();
+
+        model.addAttribute("username", userPrincipalDetails.getUserRealName());
+        model.addAttribute("email", userPrincipalDetails.getUserEamil());
+        return "createStudy";
+    }
+
+    @PostMapping("/study-create")
+    public String createStudy(@ModelAttribute("study") Study study) {
+
+        return"redirect:/study";
     }
 }
