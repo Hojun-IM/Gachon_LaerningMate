c9d618b5db8fd087fb12a0dbda452a73a2bcfa10
Hojun Im <ghwnsdla8094@gmail.com>
2024-05-13 19:32:06 +0900

    [feat] 파일 확장자 확인

--- src/main/java/com/gachon/learningmate/config/FileUploadUtil.java
+++ src/main/java/com/gachon/learningmate/config/FileUploadUtil.java
@@ -8,8 +8,18 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.Arrays;
+import java.util.List;
 
 public class FileUploadUtil {
+
+    private static final List<String> ALLOWED_EXTENSIONS = Arrays.asList("jpg", "jpeg", "png");
+
+    public static boolean isExtensionValid(MultipartFile file) {
+        String fileName = file.getOriginalFilename();
+        String fileExtension = fileName.substring(fileName.lastIndexOf(".") + 1).toLowerCase();
+        return ALLOWED_EXTENSIONS.contains(fileExtension);
+    }
 
     public static void saveFile(String uploadDir, String fileName, MultipartFile multipartFile) throws IOException {
         // 디렉토리 경로 가져오기
