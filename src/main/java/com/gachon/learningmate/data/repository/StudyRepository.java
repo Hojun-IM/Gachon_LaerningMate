4853754fb7551573c9e4038e8c24cd888a748791
Hojun Im <ghwnsdla8094@gmail.com>
2024-05-19 14:06:33 +0900

    [feat] 스터디 아이디로 존재 여부 확인 추가

--- src/main/java/com/gachon/learningmate/data/repository/StudyRepository.java
+++ src/main/java/com/gachon/learningmate/data/repository/StudyRepository.java
@@ -1,18 +1,14 @@
 package com.gachon.learningmate.data.repository;
 
-import com.gachon.learningmate.data.dto.StudyDto;
 import com.gachon.learningmate.data.entity.Study;
-import com.gachon.learningmate.data.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface StudyRepository extends JpaRepository<Study, Long> {
 
-    // User를 통해 Study 검색
-    Study findByCreatorId(User creator);
+    // 스터디 아이도로 스터디 존재 여부 확인
+    boolean existsByStudyId(int studyId);
 
     // 스터디 아이디로 스터디 검색
     Study findByStudyId(int studyId);
