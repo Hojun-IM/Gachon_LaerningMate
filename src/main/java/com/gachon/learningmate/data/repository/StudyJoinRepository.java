0b6bea179f7410b152de80eb6cb666d156ad00ff
Hojun Im <ghwnsdla8094@gmail.com>
2024-06-16 16:00:22 +0900

    [feat] studyjoin 리스트 반환 추가

--- src/main/java/com/gachon/learningmate/data/repository/StudyJoinRepository.java
+++ src/main/java/com/gachon/learningmate/data/repository/StudyJoinRepository.java
@@ -1,10 +1,15 @@
 package com.gachon.learningmate.data.repository;
 
-import com.gachon.learningmate.data.dto.StudyJoinDto;
+import com.gachon.learningmate.data.entity.Study;
 import com.gachon.learningmate.data.entity.StudyJoin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudyJoinRepository extends JpaRepository<StudyJoin, Long> {
+
+    // 스터디 아이디로 스터디 신청 검색
+    List<StudyJoin> findByStudy(Study study);
 }
