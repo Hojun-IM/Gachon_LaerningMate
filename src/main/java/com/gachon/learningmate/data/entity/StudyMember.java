95b9ae0d68c9943df4d2089dafd66100a8c458cc
Hojun Im <ghwnsdla8094@gmail.com>
2024-06-17 14:38:34 +0900

    [feat] StudyMember 엔티티 추가

---
+++ src/main/java/com/gachon/learningmate/data/entity/StudyMember.java
@@ -0,0 +1,39 @@
+package com.gachon.learningmate.data.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "study_member")
+public class StudyMember {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long memberId;
+
+    @ManyToOne
+    @JoinColumn(name = "study_id")
+    private Study study;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "join_date", nullable = false, updatable = false)
+    private Date joinDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private StudyJoinRole role;
+}
