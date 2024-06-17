4877c5b3c84251461ecad0c1e9fdcac1e8df21e8
Hojun Im <ghwnsdla8094@gmail.com>
2024-06-17 14:38:21 +0900

    [feat] 자기소개 유효성 검사 추가

--- src/main/java/com/gachon/learningmate/data/dto/StudyJoinDto.java
+++ src/main/java/com/gachon/learningmate/data/dto/StudyJoinDto.java
@@ -4,6 +4,8 @@ import com.gachon.learningmate.data.entity.Study;
 import com.gachon.learningmate.data.entity.StudyJoin;
 import com.gachon.learningmate.data.entity.StudyJoinRole;
 import com.gachon.learningmate.data.entity.User;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.util.Date;
@@ -23,6 +25,7 @@ public class StudyJoinDto {
 
     private Date joinDate;
 
+    @Size(min = 10, message = "자기소개는 최소 10글자 이상이어야 합니다.")
     private String introduction;
 
     private StudyJoinRole role;
