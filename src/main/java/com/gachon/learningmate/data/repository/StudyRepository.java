2e69eb7490fb22521a2cc22ca9052a1bdc4e624d
Hojun Im <ghwnsdla8094@gmail.com>
2024-06-23 01:32:18 +0900

    [feat] 스터디 아이디로 스터디 목록 검색 추가

--- src/main/java/com/gachon/learningmate/data/repository/StudyRepository.java
+++ src/main/java/com/gachon/learningmate/data/repository/StudyRepository.java
@@ -1,15 +1,18 @@
 package com.gachon.learningmate.data.repository;
 
 import com.gachon.learningmate.data.entity.Study;
+import com.gachon.learningmate.data.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudyRepository extends JpaRepository<Study, Long> {
 
-    // 스터디 아이도로 스터디 존재 여부 확인
-    boolean existsByStudyId(int studyId);
-
     // 스터디 아이디로 스터디 검색
     Study findByStudyId(int studyId);
+
+    // 스터디 아이디로 스터디 목록 검색
+    Page<Study> findStudyByCreatorId(User user, Pageable pageable);
 }
