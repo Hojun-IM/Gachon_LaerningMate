9e9ee84185570bddff2f4ea5c263db29c45fb036
Hojun Im <ghwnsdla8094@gmail.com>
2024-06-26 21:52:35 +0900

    [feat] 홈 화면 스터디 디스플레이 추가

--- src/main/java/com/gachon/learningmate/controller/HomeController.java
+++ src/main/java/com/gachon/learningmate/controller/HomeController.java
@@ -1,20 +1,52 @@
 package com.gachon.learningmate.controller;
 
 import com.gachon.learningmate.data.dto.UserPrincipalDetails;
+import com.gachon.learningmate.data.entity.Study;
+import com.gachon.learningmate.service.FavoriteService;
+import com.gachon.learningmate.service.StudyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class HomeController extends BaseController {
+
+    private final StudyService studyService;
+    private final FavoriteService favoriteService;
+
+    @Autowired
+    public HomeController(StudyService studyService, FavoriteService favoriteService) {
+        this.studyService = studyService;
+        this.favoriteService = favoriteService;
+    }
 
     // 홈 화면 불러오기
     @GetMapping("/home")
     public String home(Model model) {
         addUserInfoToModel(model);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        List<Study> favoriteStudies = null;
+        List<Study> recentStudies = studyService.findRecentStudy(10);
+
+        if (authentication != null && authentication.isAuthenticated() && !(authentication.getPrincipal() instanceof String && authentication.getPrincipal().equals("anonymousUser"))) {
+            UserPrincipalDetails userDetails = (UserPrincipalDetails) authentication.getPrincipal();
+            String userId = userDetails.getUsername();
+            favoriteStudies = favoriteService.findFavoriteStudyByUserId(userId);
+
+            if (favoriteStudies == null || favoriteStudies.isEmpty()) {
+                favoriteStudies = recentStudies;
+            }
+        } else {
+            favoriteStudies = recentStudies;
+        }
+
+        model.addAttribute("favoriteStudies", favoriteStudies);
+        model.addAttribute("recentStudies", recentStudies);
         return "home";
     }
 
